88d6fbd93546e8ef111e8ba1d2a8a592a6115716
jslucas <jspencerlucas@gmail.com>
2020-08-23 19:23:27 -0400

    tripCalculator: calculate total trip distance

--- src/services/tripCalculator.js
+++ src/services/tripCalculator.js
@@ -1,13 +1,15 @@
 const moment = require("moment")
 
-const totalTripTime = (time, trip) => {
-  return time + moment.duration(trip.end.diff(trip.start)).asMinutes() / 60
+const toTripTotals = (acc, trip) => {
+  const time = acc.time + moment.duration(trip.end.diff(trip.start)).asMinutes() / 60
+  const distance = acc.distance + trip.distance
+  return { time, distance }
 }
 
 const calculateDriverTotals = drivers => {
   return drivers.map(driver => {
-    const totalTimeDriven = driver.trips.reduce(totalTripTime, 0)
-    return { name: driver.name, totalTimeDriven }
+    const { time, distance } = driver.trips.reduce(toTripTotals, { time: 0, distance: 0 })
+    return { name: driver.name, distance, time }
   })
 }
 
--- src/services/tripCalculator.test.js
+++ src/services/tripCalculator.test.js
@@ -7,10 +7,14 @@ describe("tripCalculator", () => {
   const driverDan = { name: "Dan", trips: [threeHoursAtTenPointOneMph, fiveHoursAtSeventyMph] }
 
   it("totals the time for a driver's trips", () => {
-    expect(calculateDriverTotals([driverDan])).toEqual([{ name: "Dan", totalTimeDriven: 8.0 }])
+    expect(calculateDriverTotals([driverDan])).toEqual([{ name: "Dan", distance: 380.3, time: 8.0 }])
+  })
+
+  it("totals the distance of a driver's trips", () => {
+    expect(calculateDriverTotals([driverDan])).toEqual([{ name: "Dan", distance: 380.3, time: 8.0 }])
   })
 
   it("doesn't explode if driver has no trips", () => {
-    expect(calculateDriverTotals([{ name: "Dan", trips: [] }])).toEqual([{ name: "Dan", totalTimeDriven: 0 }])
+    expect(calculateDriverTotals([{ name: "Dan", trips: [] }])).toEqual([{ name: "Dan", distance: 0, time: 0 }])
   })
 })
