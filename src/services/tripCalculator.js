c1d710e002811113928840d99cef4971099d85a0
jslucas <jspencerlucas@gmail.com>
2020-08-23 19:50:31 -0400

    tripCalculator: ignore trips with avg mph < 5

--- src/services/tripCalculator.js
+++ src/services/tripCalculator.js
@@ -6,9 +6,11 @@ const toTripTotals = (acc, trip) => {
   return { time, distance }
 }
 
+const ineligibleTrip = trip => !(trip.distance / (moment.duration(trip.end.diff(trip.start)).asMinutes() / 60) < 5)
+
 const calculateDriverTotals = drivers => {
   return drivers.map(driver => {
-    const { time, distance } = driver.trips.reduce(toTripTotals, { time: 0, distance: 0 })
+    const { time, distance } = driver.trips.filter(ineligibleTrip).reduce(toTripTotals, { time: 0, distance: 0 })
     const averageMph = distance && time ? distance / time : 0
     return { name: driver.name, distance, averageMph }
   })
--- src/services/tripCalculator.test.js
+++ src/services/tripCalculator.test.js
@@ -4,6 +4,7 @@ const { toMoment } = require("../utils")
 describe("tripCalculator", () => {
   const threeHoursAtTenPointOneMph = { start: toMoment("07:00"), end: toMoment("10:00"), distance: 30.3 }
   const fiveHoursAtSeventyMph = { start: toMoment("05:00"), end: toMoment("10:00"), distance: 350 }
+  const hikingTrip = { start: toMoment("06:20"), end: toMoment("9:55"), distance: 4.9 }
   const driverDan = { name: "Dan", trips: [threeHoursAtTenPointOneMph, fiveHoursAtSeventyMph] }
 
   it("totals the distance of a driver's trips", () => {
@@ -12,6 +13,14 @@ describe("tripCalculator", () => {
 
   it("averages the overall speed", () => {
     expect(calculateDriverTotals([driverDan])).toEqual([{ name: "Dan", distance: 380.3, averageMph: 47.5375 }])
+  })
+
+  it("ignores trips that average < 5 mph", () => {
+    const driverDanWithHikingTrip = { ...driverDan, trips: [hikingTrip, ...driverDan.trips] }
+
+    expect(calculateDriverTotals([driverDanWithHikingTrip])).toEqual([
+      { name: "Dan", distance: 380.3, averageMph: 47.5375 }
+    ])
   })
 
   it("doesn't explode if driver has no trips", () => {
