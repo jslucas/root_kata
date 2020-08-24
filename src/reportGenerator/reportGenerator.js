ddfcadcc1bac9be43eabc10d04437beec8b37d88
jslucas <jspencerlucas@gmail.com>
2020-08-23 21:32:15 -0400

    reportGenerator: sort report by drivers' distance (descending)

--- src/reportGenerator/reportGenerator.js
+++ src/reportGenerator/reportGenerator.js
@@ -1,5 +1,9 @@
-const generateReport = data => {
-  const report = data
+const R = require("ramda")
+
+const byDistance = R.descend(R.prop("distance"))
+
+const generateReport = drivers => {
+  const report = R.sort(byDistance)(drivers)
     .map(driver => {
       if (!driver.averageMph) {
         return `${driver.name}: 0 miles`
@@ -7,6 +11,7 @@ const generateReport = data => {
       return `${driver.name}: ${Math.round(driver.distance)} miles @ ${Math.round(driver.averageMph)} mph`
     })
     .join("\n")
+
   return report
 }
 
--- src/reportGenerator/reportGenerator.test.js
+++ src/reportGenerator/reportGenerator.test.js
@@ -10,4 +10,17 @@ describe("reportGenerator", () => {
     const driver = { name: "Dan", distance: 0, averageMph: 0 }
     expect(generateReport([driver])).toEqual("Dan: 0 miles")
   })
+
+  it("returns formatted report sorted by distance descending", () => {
+    const drivers = [
+      { name: "Dan", distance: 39.1, averageMph: 46.92000000000001 },
+      { name: "Kumi", distance: 0, averageMph: 0 },
+      { name: "Lauren", distance: 42, averageMph: 33.6 },
+      { name: "Seven", distance: 44, averageMph: 55.46 },
+      { name: "Case", distance: 256, averageMph: 99.6 }
+    ]
+    expect(generateReport(drivers)).toEqual(
+      "Case: 256 miles @ 100 mph\nSeven: 44 miles @ 55 mph\nLauren: 42 miles @ 34 mph\nDan: 39 miles @ 47 mph\nKumi: 0 miles"
+    )
+  })
 })
