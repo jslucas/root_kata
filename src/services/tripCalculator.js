b148efef970c9dd79f5149cb954ddebd4b278a83
jslucas <jspencerlucas@gmail.com>
2020-08-23 20:09:20 -0400

    tripCalculator: refactor, remove now redundant test case

--- src/services/tripCalculator.js
+++ src/services/tripCalculator.js
@@ -1,5 +1,8 @@
 const moment = require("moment")
 
+const MIN_MPH = 5
+const MAX_MPH = 100
+
 const tripTime = trip => moment.duration(trip.end.diff(trip.start)).asMinutes() / 60
 
 const toTripTotals = (acc, trip) => {
@@ -8,12 +11,13 @@ const toTripTotals = (acc, trip) => {
   return { time, distance }
 }
 
-const ineligibleTrip = trip => !(trip.distance / tripTime(trip) < 5 || trip.distance / tripTime(trip) > 100)
+const isEligibleTrip = trip => !(trip.distance / tripTime(trip) < MIN_MPH || trip.distance / tripTime(trip) > MAX_MPH)
 
 const calculateDriverTotals = drivers => {
   return drivers.map(driver => {
-    const { time, distance } = driver.trips.filter(ineligibleTrip).reduce(toTripTotals, { time: 0, distance: 0 })
+    const { time, distance } = driver.trips.filter(isEligibleTrip).reduce(toTripTotals, { time: 0, distance: 0 })
     const averageMph = distance && time ? distance / time : 0
+
     return { name: driver.name, distance, averageMph }
   })
 }
--- src/services/tripCalculator.test.js
+++ src/services/tripCalculator.test.js
@@ -9,39 +9,34 @@ describe("tripCalculator", () => {
       { start: toMoment("05:00"), end: toMoment("10:00"), distance: 350 }
     ]
   }
+  const expectedDriver = { name: "Dan", distance: 380.3, averageMph: 47.5375 }
 
   const hikingTrip = { start: toMoment("06:20"), end: toMoment("9:55"), distance: 4.9 }
   const tripToSpace = { start: toMoment("01:05"), end: toMoment("1:07"), distance: 849760000 }
 
-  it("totals the distance of a driver's trips", () => {
-    expect(calculateDriverTotals([driver])).toEqual([{ name: "Dan", distance: 380.3, averageMph: 47.5375 }])
-  })
-
-  it("averages the overall speed", () => {
-    expect(calculateDriverTotals([driver])).toEqual([{ name: "Dan", distance: 380.3, averageMph: 47.5375 }])
+  it("calculates the total distance and avg mph of a driver's trips", () => {
+    expect(calculateDriverTotals([driver])).toEqual([expectedDriver])
   })
 
   it("ignores trips that average < 5 mph", () => {
     const driverWithHikingTrip = { ...driver, trips: [hikingTrip, ...driver.trips] }
 
-    expect(calculateDriverTotals([driverWithHikingTrip])).toEqual([
-      { name: "Dan", distance: 380.3, averageMph: 47.5375 }
-    ])
+    expect(calculateDriverTotals([driverWithHikingTrip])).toEqual([expectedDriver])
   })
 
   it("ignores trips that average > 100 mph", () => {
     const driverWithTripToSpace = { ...driver, trips: [tripToSpace, ...driver.trips] }
-    expect(calculateDriverTotals([driverWithTripToSpace])).toEqual([
-      { name: "Dan", distance: 380.3, averageMph: 47.5375 }
-    ])
+
+    expect(calculateDriverTotals([driverWithTripToSpace])).toEqual([expectedDriver])
   })
 
-  it("handles driver with only ineligible trips", () => {
+  it("handles drivers with only ineligible trips", () => {
     const driverWithNoEligibleTrips = { ...driver, trips: [hikingTrip, tripToSpace] }
+
     expect(calculateDriverTotals([driverWithNoEligibleTrips])).toEqual([{ name: "Dan", distance: 0, averageMph: 0 }])
   })
 
-  it("doesn't explode if driver has no trips", () => {
+  it("handles drivers with no trips", () => {
     expect(calculateDriverTotals([{ name: "Dan", trips: [] }])).toEqual([{ name: "Dan", distance: 0, averageMph: 0 }])
   })
 })
